799c30e7ce9838de4e6cd7f232c8a30120abfd09
Yuri Kitchin <ykitchin@gmail.com>
2021-07-14 20:08:34 +0800

    add roles and emloyees to seed db

--- seed.sql
+++ seed.sql
@@ -35,3 +35,46 @@ INSERT INTO department (department_name)
 VALUES ("Front-end Development");
 INSERT INTO department (department_name)
 VALUES ("Back-end Development");
+
+-- roles
+INSERT INTO roles (id, title, salary, department_id)  
+VALUES (1, 'Recptionist', '35000', 1);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (2, 'HR administrator', '50000', 1);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (3 , 'Office Manager', '150000', 1);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (4, 'Junior designer', '50000', 2);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (5, 'Seniour designer', '70000', 2);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (6, 'Design Team Leader', '100000', 2);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (7, 'Junior Dev', '50000', 3);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (8, 'Senior Dev', '70000', 3);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (9, 'Development Team Leader', '100000', 3);
+
+-- employees
+-- Administration
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (1, 'John', "Doe", 3, NULL ); 
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (4, 'lenny', "kravits", 1, 1 ); 
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (5, 'sarah', "marshall", 2, 1 ); 
+-- front end
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (2, 'Jane', "Doe", 6, NULL ); 
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (6, 'jenny', "savage", 4, 2 ); 
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (7, 'kayla', "yildiz", 5, 2 ); 
+-- backend 
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (3, 'Kenny', "Powers", 9, NULL );
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (8, 'lara', "croft", 7, 3 );
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) 
+VALUES (9, 'Joe', "Exotic", 8, 3 );  
