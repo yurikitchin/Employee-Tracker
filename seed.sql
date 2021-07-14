4499e4bc9d83c2e972f8abaa72106aa051318ccb
Yuri Kitchin <ykitchin@gmail.com>
2021-07-14 19:13:18 +0800

    fix syntax error

--- seed.sql
+++ seed.sql
@@ -5,6 +5,7 @@ CREATE TABLE department (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   department_name VARCHAR(30) NOT NULL
 );
+
 CREATE TABLE roles (
   id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30),
@@ -12,11 +13,12 @@ CREATE TABLE roles (
   department_id INT, 
   PRIMARY KEY (id)
 );
+
 CREATE TABLE employees (
   id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT 
+  manager_id INT,
   PRIMARY KEY (id)
 );
