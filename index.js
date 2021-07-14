bdcbb1b06d932a80fdb5adb19fea54e287017f4b
Yuri Kitchin <ykitchin@gmail.com>
2021-07-14 20:24:17 +0800

    install npm packages, create connection to mysql

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 //require dependencies
 const mysql = require('mysql2')
 const inquirer = require('inquirer')
+
+//creat connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'ykitchkey1!',
+    database: 'employee_trackerdb',
+  });
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+  });
+  
